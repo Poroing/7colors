f6bd3ce652cacd865e9c6796b1ba89158338c989
Poroing <jean-18-09@hotmail.fr>
2017-10-04 17:36:14 +0200

    Initializing board

--- 7colors.c
+++ 7colors.c
@@ -1,9 +1,15 @@
 /* Template of the 7 wonders of the world of the 7 colors assigment. */
 
 #include <stdio.h>     /* printf */
+#include <time.h>
+#include <stdlib.h>
 
 /* We want a 30x30 board game by default */
 #define BOARD_SIZE 30
+#define COLOR_NUMBER 7
+#define DOWN_LEFT_PLAYER 'v'
+#define TOP_RIGHT_PLAYER '^'
+#define EMPTY 0
 
 /** Represent the actual current board game
  *
@@ -12,6 +18,14 @@
  *     For this first assignment, no dinosaure will get you if you do that.
  */
 char board[BOARD_SIZE * BOARD_SIZE] = { 0 }; // Filled with zeros
+
+struct cell
+{
+    int x;
+    int y;
+};
+
+typedef struct cell cell;
 
 /** Retrieves the color of a given board cell */
 char get_cell(int x, int y)
@@ -41,12 +55,57 @@ void print_board(void)
     }
 }
 
+void initialize_random(void)
+{
+    srand(time(NULL));
+}
+
+/** Return number is in [a, b[ */
+int get_random_number_between(int a, int b)
+{
+    return rand() % (b - a) + a;
+}
+
+char get_color(size_t id)
+{
+    return id + 'A';
+}
+
+char get_player_of_starting_cell(cell c)
+{
+   if (c.x == 0 && c.y == 0)
+        return DOWN_LEFT_PLAYER;
+   if (c.x == BOARD_SIZE - 1 && c.y == BOARD_SIZE - 1)
+        return TOP_RIGHT_PLAYER;
+    return EMPTY;
+}
+
+void initialize_board(void)
+{
+    char player;
+    cell current_cell;
+    for (int i = 0; i < BOARD_SIZE ; ++i) {
+        for (int j = 0; j < BOARD_SIZE; ++j) {
+            current_cell.x = i;
+            current_cell.y = j;
+            player = get_player_of_starting_cell(current_cell);
+            if (player == EMPTY)
+                set_cell(i, j, get_color(get_random_number_between(0, 7)));
+            else
+                set_cell(i, j, player);
+        }
+    }
+    
+}
+
 /** Program entry point */
 int main(void)
 {
     printf("\n\nWelcome to the 7 wonders of the world of the 7 colors\n"
 	   "*****************************************************\n\n"
 	   "Current board state:\n");
+    initialize_random();
+    initialize_board();
 
     print_board();
 
